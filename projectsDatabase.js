0ee09ede51c42dcaff1dc51c94a293036f193cd8
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-05-26 12:41:31 +0300

    db

---
+++ projectsDatabase.js
@@ -0,0 +1,62 @@
+const projectsDatabase = [
+  {
+    id: 1,
+    name: 'Сапер',
+    description: 'Игра сапер',
+    img: '',
+  },
+  {
+    id: 2,
+    name: 'Filament',
+    description: 'Веб-сайт для видеопродакшена',
+    img: '',
+  },
+  {
+    id: 3,
+    name: 'Cool-Buy',
+    description: 'Тестовый интернет магазин',
+    img: '',
+  },
+  {
+    id: 4,
+    name: 'To-Do-To-Do List',
+    description: 'Список задач, где можно создавать подзадачи',
+    img: '',
+  },
+  {
+    id: 5,
+    name: 'Savewatch',
+    description: 'Секундомер с возможностью сохранять время',
+    img: '',
+  },
+  {
+    id: 6,
+    name: 'Random Number Generator',
+    description: 'Генератор случайных чисел',
+    img: '',
+  },
+  {
+    id: 7,
+    name: "Conway's Game of Life",
+    description: `Простой симулятор игры "Жизнь"`,
+    img: '',
+  },
+  {
+    id: 8,
+    name: 'Wordborne',
+    description: 'Генератор слов',
+    img: '',
+  },
+  {
+    id: 9,
+    name: 'Weather App',
+    description: 'Прогноз погоды',
+    img: '',
+  },
+  {
+    id: 10,
+    name: 'Monty Hall',
+    description: 'Простая игра основанная на проблеме Монти Холла',
+    img: '',
+  },
+]
