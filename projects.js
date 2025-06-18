ad1da05c17b05e98c8f176b5c1f8018d1a33ccc0
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-06-18 14:52:30 +0300

    projects commit

--- projects.js
+++ projects.js
@@ -6,8 +6,8 @@ const projectCards = projectsDatabase.map((project) => {
   return `
       <div class="project-card">
         <img src="images/${project.img}" alt="${project.name}">
-        <h3>${project.name}</h3>
-        <p>${project.description}</p>
+        <h3 class="project-name">${project.name}</h3>
+        <p class="project-p">${project.description}</p>
       </div>
     `
 })
