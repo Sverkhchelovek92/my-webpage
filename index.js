9cedb6f596d40e4203bf39c322c055884a1c77c9
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-07-18 12:32:44 +0300

    header

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 document.querySelector('.current-year').textContent = new Date().getFullYear()
+
+fetch('header.html')
+  .then((res) => res.text())
+  .then((data) => {
+    document.querySelector('.header').innerHTML = data
+  })
