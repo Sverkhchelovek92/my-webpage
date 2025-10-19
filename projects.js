e42db1897d35afe5dba376804b6cd60eac629b0a
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-10-19 12:57:12 +0300

    projects page

--- projects.js
+++ projects.js
@@ -5,9 +5,15 @@ const projectsContainer = document.getElementById('projects-container')
 const projectCards = projectsDatabase.map((project) => {
   return `
       <div class="project-card">
-        <img src="images/${project.img}" alt="${project.name}">
+      <div class="project-thumb">
+      <img src="images/${project.img}" alt="${project.name}">
+      </div>
+        <div class="project-info">
         <h3 class="project-name">${project.name}</h3>
         <p class="project-p">${project.description}</p>
+        <a class="project-link" href="${project.gitlink}" target="_blank">[ View on GitHub ]</a>
+        </div>
+        
       </div>
     `
 })
