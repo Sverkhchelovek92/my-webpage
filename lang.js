d916b33ae02edbdd1b79b319eaec6a97b35eef96
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-11-11 12:18:47 +0300

    language stuff

---
+++ lang.js
@@ -0,0 +1,24 @@
+const translations = {
+  ru: {
+    name: 'Мирослав Гурняк',
+    subtitle: 'JavaScript разработчик',
+    about_title: 'Обо мне',
+    about_text: 'Привет, меня зовут Мирослав, я фронтенд-разработчик.',
+    links_title: 'Ссылки',
+    projects_title: 'Мои проекты',
+
+    about_link: 'Обо мне',
+    projects_link: 'Проекты',
+  },
+  en: {
+    name: 'Miroslav Gurnyak',
+    subtitle: 'JavaScript Developer',
+    about_title: 'About Me',
+    about_text:
+      'Hello, my name is Miroslav and I am front-end javascript developer.',
+    links_title: 'Links',
+    projects_title: 'My projects',
+    about_link: 'About',
+    projects_link: 'Projects',
+  },
+}
